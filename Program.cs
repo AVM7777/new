512fa78c3989d1ed8fef3f414071df0030a279e2
AVM7777 <159691628+AVM7777@users.noreply.github.com>
2024-05-11 23:57:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿System.Console.WriteLine("Введите строки для массива через запятую");
+string[] initialArray = Console.ReadLine().Split(',');
+string[] newArray = FilterArrayByLength(initialArray, 3);
+System.Console.WriteLine("Новый массив строк с длиной меньше или равной 3 символам:");
+foreach (string str in newArray)
+{
+    Console.Write(string.Join(", ", str + " "));
+}
+
+string[] FilterArrayByLength(string[] array, int maxLength)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= maxLength)
+        {
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    int i = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= maxLength)
+        {
+            result[i] = $"\"{str}\"";
+            i++;
+        }
+    }
+
+    return result;
+}
